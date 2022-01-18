4f9310c17e94c337c7ed71d877135ae14855d853
UnclassedPenguin <tvanden@live.com>
2022-01-18 12:30:32 -0700

    clean up js

--- assets/js/setRandomColor.js
+++ assets/js/setRandomColor.js
@@ -19,30 +19,16 @@ const colors = ["#81EF9E",
                 "#5EE4A5",
                 "#5EDFE4"]
 
-function getRandomColor() {
-  return colors[Math.floor(Math.random() * colors.length)];
-}
-
-
 function setRandomLinkColor() {
   Array.from(document.getElementsByClassName("randomcolor")).forEach((item) => {
-    item.style.color = getRandomColor();
+    item.style.color = colors[Math.floor(Math.random() * colors.length)];
   });
 }
 
-function getRandomHeaderColor() {
+function setRandomHeaderColor() {
 	var r = document.querySelector(':root');
 	r.style.setProperty('--rand', colors[Math.floor(Math.random() * colors.length)])
 }
 
-//function getRandomColorHeader() {
-	//var r = document.querySelector(':root');
-	//var length = colors.length
-	//var rand = Math.floor((Math.random() * length) + 0);
-	//r.style.setProperty('--rand', colors[rand])
-	//return colors[rand];
-//}
-
-
 setRandomLinkColor();
-getRandomHeaderColor();
+setRandomHeaderColor();
